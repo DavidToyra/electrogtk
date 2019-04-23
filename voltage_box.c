51fd9ff35df909658f777adc2006a18d59ecd12b
David <davidtoyra96@gmail.com>
2019-04-23 16:14:30 +0200

    fixed voltage box

--- voltage_box.c
+++ voltage_box.c
@@ -12,13 +12,23 @@ VoltageWidgets *voltage_box_new(void)
 {
 	
 	VoltageWidgets *voltageInfo;
-	printf("after struct create\n");
-	voltageInfo->voltage = gtk_entry_with_name_new("voltage");
+
+	//set widget name
+	GtkWidget* entry;
+	entry = gtk_entry_new();
+	gtk_widget_set_name(entry, "voltage");
+	
+	voltageInfo->voltage = entry;
 	printf("easypeasy\n");
 	voltageInfo->voltage_box = gtk_vbox_new(FALSE, 2);
 	printf("filled the struct with shit\n");
-	add_widget_with_label_vbox(GTK_CONTAINER(voltageInfo->voltage_box), 
-	"Voltage (V):", voltageInfo->voltage);
+	//set label box
+	GtkWidget *label = gtk_label_new("Voltage (V): ");
+  	GtkWidget *hbox = gtk_hbox_new(TRUE,2);
+  	gtk_container_add(GTK_CONTAINER(hbox), label);
+  	gtk_container_add(GTK_CONTAINER(hbox), voltageInfo->voltage);
+  	gtk_container_add(voltageInfo->voltage_box, hbox);
+	
 	printf("added label box\n");
 	return voltageInfo;
 }
